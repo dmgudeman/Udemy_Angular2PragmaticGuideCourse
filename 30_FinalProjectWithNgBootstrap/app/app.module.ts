8300054c752d76947c304ff6236897148ab49e7b
davidmgudeman <davidmgudeman@gmail.com>
2017-01-20 10:45:07 -0800

    The navbar is working with inlcuding in the index.html as mosh did not through system.config.js and not using ng-bootstrap

--- 30_FinalProjectWithNgBootstrap/app/app.module.ts
+++ 30_FinalProjectWithNgBootstrap/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    NgbModule.forRoot()
+   
      ],
   declarations: [ 
     AppComponent,
