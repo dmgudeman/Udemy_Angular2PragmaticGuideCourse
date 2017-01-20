f6a3b1b511334a77da305fcaa8c15aeb7dfd9251
davidmgudeman <davidmgudeman@gmail.com>
2017-01-20 10:22:34 -0800

    Got angular-bootstrap working: instructions: 	https://ng-bootstrap.github.io/#/getting-started

---
+++ 30_FinalProjectWithNgBootstrap/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'navbar',
+   templateUrl: 'app/navbar/navbar.component.html'
+
+})
+export class NavbarComponent {
+   constructor(){
+
+   }
+
+   OnInit() {
+
+   }
+}
