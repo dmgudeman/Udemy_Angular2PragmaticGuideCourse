082474b598c4d04b966f99ea586746ddd7c075f3
davidmgudeman <davidmgudeman@gmail.com>
2017-01-21 03:00:43 -0800

    Routes for 30 are working

--- 30_FinalProjectWithNgBootstrap/app/app.component.ts
+++ 30_FinalProjectWithNgBootstrap/app/app.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-      <h1>Hello {{name}}</h1>
-      <navbar></navbar>
-      <posts></posts>
-      <users></users>
-      <not-found></not-found>
+   
+   <navbar></navbar>
+  
+      <ul>
+        <li><a routerLink="">Home</a></li>
+        <li><a routerLink="posts">Posts</a></li>
+        <li><a routerLink="users">Users</a></li>
+      </ul>
+       <router-outlet></router-outlet>
       `,
 })
 export class AppComponent  { name = 'Angular'; }
--- 30_FinalProjectWithNgBootstrap/app/app.module.ts
+++ 30_FinalProjectWithNgBootstrap/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { PostsComponent } from './posts/posts.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { routing } from './app.routing';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
+    routing
    
      ],
   declarations: [ 
     AppComponent,
     NavbarComponent,
+    HomeComponent,
     UsersComponent,
     PostsComponent,
     NotFoundComponent,
