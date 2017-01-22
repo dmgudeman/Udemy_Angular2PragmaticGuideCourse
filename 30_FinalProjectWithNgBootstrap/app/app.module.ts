7152cab3cdd89f6fc025365ccd4b14003f7b0346
davidmgudeman <davidmgudeman@gmail.com>
2017-01-22 14:20:53 -0800

    30 refactored to make a UserModule

--- 30_FinalProjectWithNgBootstrap/app/app.module.ts
+++ 30_FinalProjectWithNgBootstrap/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { routing } from './app.routing';
-import { usersRouting } from './users/users.routing';
+// import { usersRouting } from './users/users.routing';
 import { UsersModule } from './users/users.module';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { UsersExperimentalComponent } from './users/users-experimental.component';
-import { UserDetailsComponent } from './users/user-details.component';
+// import { UsersComponent } from './users/users.component';
+// import { UsersExperimentalComponent } from './users/users-experimental.component';
+// import { UserDetailsComponent } from './users/user-details.component';
 import { PostsComponent } from './posts/posts.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-import { UsersService } from './users/users.service';
-import { UsersExperimentalService } from './users/users-experimental.service';
+// import { UsersService } from './users/users.service';
+// import { UsersExperimentalService } from './users/users-experimental.service';
 
 @NgModule({
   imports:      [ 
@@ -23,22 +23,22 @@ import { UsersExperimentalService } from './users/users-experimental.service';
     HttpModule,
     JsonpModule,
     UsersModule,
-    usersRouting,
+    // usersRouting,
     routing,
      ],
   declarations: [ 
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    UsersComponent,
-    UsersExperimentalComponent,
-    UserDetailsComponent,
+    // UsersComponent,
+    // UsersExperimentalComponent,
+    // UserDetailsComponent,
     PostsComponent,
     NotFoundComponent,
     ],
   providers: [
-    UsersService,
-    UsersExperimentalService
+    // UsersService,
+    // UsersExperimentalService
   ],
   bootstrap:    [ AppComponent ]
 })
