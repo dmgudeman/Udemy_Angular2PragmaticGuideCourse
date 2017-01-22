5efe8f9624610abf92bfab2053b8b6e03c392011
davidmgudeman <davidmgudeman@gmail.com>
2017-01-22 13:55:47 -0800

    29 did some work on the bootstrap configuration including the index.html

---
+++ 30_FinalProjectWithNgBootstrap/app/users/users.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { UsersComponent } from './users.component';
+import { UsersExperimentalComponent } from './users-experimental.component';
+import { UserDetailsComponent } from './user-details.component';
+
+import { UsersService } from './users.service';
+import { UsersExperimentalService } from './users-experimental.service';
+import { usersRouting } from './users.routing';
+
+@NgModule({
+   imports: [
+      usersRouting,
+   ],
+   declarations: [
+      UsersComponent,
+      UsersExperimentalComponent,
+      UserDetailsComponent
+   ],
+   exports: [
+      UsersComponent,
+      UsersExperimentalComponent,
+      UserDetailsComponent
+   ],
+   providers: [
+      UsersService,
+      UsersExperimentalService
+   ],
+
+})
+export class UsersModule { }
