98c85323d971ee58cccf2dbb29b248e4a7943271
davidmgudeman <davidmgudeman@gmail.com>
2017-01-22 14:30:21 -0800

    30 refactored the app-experimental.component to have its own template

--- 30_FinalProjectWithNgBootstrap/app/users/users-experimental.component.ts
+++ 30_FinalProjectWithNgBootstrap/app/users/users-experimental.component.ts
@@ -1,40 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Http,} from '@angular/http';
+import { Http, } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/map'
 import { UsersExperimentalService } from './users-experimental.service';
 
-
 @Component({
       selector: 'users-experimental',
-      
-      template: `
-      <h4>Users Experimental </h4>
-         
-    <table class=" table table-bordered myTable">
-   <thead>
-      <tr>
-         <th>User</th>
-         <th>Email</th>
-         <th>Edit</th>
-         <th>Delete</th>
-      </tr>
-   </thead>
-
-   <tbody>
-
-      <tr *ngFor="let user of users">
-      <td> {{user.name}}</td>
-      <td> {{user.email}}</td>
-      <td> <i class="glyphicon glyphicon-edit"></i></td>
-      <td> <i class="glyphicon glyphicon-remove"></i></td>
-      </tr>
-   </tbody>
-
-
-</table>
-
-      `
+      templateUrl: 'app/users/users-experimental.component.html'
 })
 export class UsersExperimentalComponent implements OnInit {
       errorMessage: string;
@@ -42,21 +14,20 @@ export class UsersExperimentalComponent implements OnInit {
       listId: string;
       listName: string;
       mode = 'Observable';
-      
-      constructor(private _route: ActivatedRoute, private usersExperimentalService: UsersExperimentalService) {     
-       }
+
+      constructor(private _route: ActivatedRoute, private usersExperimentalService: UsersExperimentalService) {
+      }
 
       ngOnInit() {
             this.getUsers(); {
             }
-            console.log(this.users);
       }
 
-      getUsers(){
+      getUsers() {
             this.usersExperimentalService.getUsers()
                   .subscribe(
-                      users =>this.users = users,
-                      error => this.errorMessage = <any>error
+                  users => this.users = users,
+                  error => this.errorMessage = <any>error
                   );
       }
 }
